4c2878efe345e3dca7bb0e7a4b1795253b6b45d4
오하늘 <nebulass@naver.com>
2022-06-22 10:49:34 +0900

    useRef로 참조하여 애니메이션 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './scss/style.scss';
 
-import { Canvas } from 'react-three-fiber';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { useRef } from 'react';
 
 const Box = () => {
+	const box = useRef(null);
+
+	//useFrame은 화면 주사율에 맞게 프레임별로 정보값 제공
+	//모션 작업을 하고 싶을 때 해당 오브제에 모션 작업 구문 주로 설정
+	useFrame((state) => {
+		box.current.rotation.x += 0.01;
+		box.current.rotation.y += 0.01;
+	});
 	return (
-		<mesh>
+		<mesh ref={box}>
 			<boxBufferGeometry />
 			<meshBasicMaterial color='hotpink' />
 		</mesh>
