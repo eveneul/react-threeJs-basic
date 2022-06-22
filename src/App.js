4bcfef390e06d88f9108e975ee171740d2ccc289
오하늘 <nebulass@naver.com>
2022-06-22 11:47:10 +0900

    포인트를 연결해 라인 생성

--- src/App.js
+++ src/App.js
@@ -35,10 +35,23 @@ function App() {
 			<figure>
 				{/* camera 위치값 [x, y, z] */}
 				<Canvas style={{ background: '#000' }} camera={{ position: [3, 3, 3] }}>
-					<Box position={[1, -1, 0]} />
+					{/* <Box position={[1, -1, 0]} /> */}
 					<Orbit />
 					<axesHelper args={[5]} />
 					{/* 가이드 축 보이게 함 */}
+
+					{/* 컴포넌트 이름에 따라서 렌더링 방식이 달라짐
+					Points: 포트 생성
+					line: 포인트를 선으로 연결
+					mesh: 라인을 면으로 생성 */}
+
+					<points>
+						<geometry>
+							<vector3 attachArray='vertices' />
+							<vector3 attachArray='vertices' args={[0, 1, 1]} />
+							<vector3 attachArray='vertices' args={[0, 1, -1]} />
+						</geometry>
+					</points>
 				</Canvas>
 			</figure>
 		</section>
