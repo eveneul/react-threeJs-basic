76d3a16c699b48ddf1f490ccce00b24af78e3f3d
오하늘 <nebulass@naver.com>
2022-06-23 14:08:22 +0900

    velocity로 반응도 조절

--- src/components/Dragable.js
+++ src/components/Dragable.js
@@ -27,6 +27,9 @@ function Dragable(props) {
 			//해당 박스를 드래그할 때 오브젝트 요소에 연결되어 있는 물리법칙 속성값 api 출력
 			// 드래그 할 때 현재 오브젝트 요소의 api-position값을 기존 물리법칙 속성값에 복사해서 치환
 			e.object.api.position.copy(e.object.position);
+
+			e.object.api.velocity.set(0, 0, 0);
+			// 오브젝트의 반응도
 		});
 	}, [children]);
 	return (
